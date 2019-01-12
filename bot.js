66cb0ddb3ce312cf94756b0eeaef1605d115aa10
Egram404 <41884139+Egram404@users.noreply.github.com>
2019-01-13 02:52:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,17 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-//const token = require("./settings.json").token;;
-const fs = require("fs");
 
-client.on("ready",() => {
-	console.log("I'm online supreme");
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('guildMemberAdd', member => {
-	console.log('User ' + member.user.username + ' as join the server');
-	member.guild.channels.get('480929688605425694')send(`-Ꮃelcome ${member.user.username} To **__Versače.__**`)
+  
+  const channel = member.guild.channels.find(ch => ch.name === 'versace');
+ 
+  if (!channel) return;
+
+  channel.send(`-Ꮃelcome ${member.user.username} To **__Versače.__**`);
 });
 
 client.login(process.env.BOT_TOKEN);
